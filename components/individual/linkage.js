8bb005f89f2e2988d507c33a1358e24053f6553a
VinayVidhehi <vinayvidhehi@gmail.com>
2023-12-31 06:26:53 +0530

    functions for new database

--- components/individual/linkage.js
+++ components/individual/linkage.js
@@ -8,12 +8,23 @@ const linkage = () => {
   const [message, setMessage] = useState("");
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const response = await axios.get(
-      `https://bat-project.onrender.com/values?option=${selectedOption}&value=${searchFor}`
-    );
-    const responseArray = response.data.results[0];
-    console.log("response is ", responseArray);
-    setMessage(responseArray.first_name);
+    try {
+      const response = await axios.get(
+        `https://bat-project.onrender.com/values?option=${selectedOption}&value=${searchFor}`
+      );
+
+      const responseArray = response.data.results[0];
+      
+      // Check if the response contains any data
+      if (Object.keys(responseArray).length === 0) {
+        setMessage("No match found. Please try another value.");
+      } else {
+        setMessage(responseArray);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setMessage("Error fetching data. Please try again with different values that might match.");
+    }
   };
 
   return (
@@ -22,36 +33,57 @@ const linkage = () => {
         <h2 className="text-4xl font-bold text-gray-900 mb-8">
           Linkage Mapping
         </h2>
-        <div className="flex gap-5">
-          <form onSubmit={handleFormSubmit}>
-            <label>
+        <div className="flex flex-col items-center justify-center gap-4 bg-gray-100 p-4 rounded-lg shadow-md">
+          <form onSubmit={handleFormSubmit} className="w-full">
+            <label className="block text-gray-700 font-bold mb-2">
               Select an Option:
               <Select
                 value={selectedOption}
                 onChange={(e) => setSelectedOption(e)}
+                defaultValue="id"
+                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               >
                 <option value="genotype">genotype</option>
                 <option value="phenotype">phenotype</option>
-                <option value="id">from</option>
-                <option value="first_name">to</option>
+                <option value="id">id</option>
+                <option value="sl">sl</option>
+                <option value="ssr_motif">ssr motif</option>
+                <option value="Molecular_Function">Molecular function</option>
+                <option value="Gene_ontology_IDs">Gene ontology IDs</option>
               </Select>
             </label>
-            <br />
-            <label>enter the "{selectedOption}" you want to search for </label>
-            <input
-              value={searchFor}
-              onChange={(e) => setSearchFor(e.target.value)}
-            />
-            <br />
+            <label className="block text-gray-700 font-bold mb-2">
+              Enter the "{selectedOption}" you want to search for:
+              <input
+                value={searchFor}
+                onChange={(e) => setSearchFor(e.target.value)}
+                className="mt-1 block w-full pl-3 pr-10 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </label>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
             >
               Submit
             </button>
           </form>
           <br />
-          <p>{message}</p>
+          {message && (
+            <div>
+              <h3 className="text-xl font-bold mb-2">Response:</h3>
+              {typeof message === "string" ? (
+                <p>{message}</p>
+              ) : (
+                <ul>
+                  {Object.entries(message).map(([key, value]) => (
+                    <li key={key}>
+                      <strong>{key}:</strong> {value}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
